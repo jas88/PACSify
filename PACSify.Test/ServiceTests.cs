6c544e2cafc8aa24b0604057066b0daf77e82401
James A Sutherland <>
2022-10-16 18:14:54 -0500

    Add trivial test of starting while cancelled

--- PACSify.Test/ServiceTests.cs
+++ PACSify.Test/ServiceTests.cs
@@ -12,6 +12,11 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var cancelTokenSource = new CancellationTokenSource();
+        cancelTokenSource.Cancel(true);
+        var stoppingToken=cancelTokenSource.Token;
+        using var server = DicomServerFactory.Create<StoreScp>(1104);
+        await Task.Run(() => stoppingToken.WaitHandle.WaitOne(), stoppingToken);
         Assert.Pass();
     }
 }
